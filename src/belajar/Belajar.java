ba6202754a5f3a1144aa3defef1b9bbc835599ce
musfiratiratih <musfiratiratih@gmail.com>
2020-10-19 15:37:02 +0700

    hore berhasil

---
+++ src/belajar/Belajar.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package belajar;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author musfi
+ */
+public class Belajar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Deklarasi variabel
+        int a = 5;
+        int b = 10;
+        
+        //rumus
+        int c = a+b;
+
+        
+        //Tampilkan perhitungan
+        
+        System.out.println("Hasilnya adalah "+c);
+    }
+}
